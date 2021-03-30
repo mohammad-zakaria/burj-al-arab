c72cc51bc5aa400da873903a70d1c3c60c2eb27e
Mohammad Zakaria <zakaria.eee.iiuc@gmail.com>
2021-03-31 00:38:04 +0600

    done upto mongo db

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDXUD7sP8xBoytVhvgnoLOSxeFrCOHw__o",
+  authDomain: "burj-al-arab-after-auth-zack.firebaseapp.com",
+  projectId: "burj-al-arab-after-auth-zack",
+  storageBucket: "burj-al-arab-after-auth-zack.appspot.com",
+  messagingSenderId: "266297431633",
+  appId: "1:266297431633:web:ec037bc66c2629d6c74a70"
+};
+  export default firebaseConfig;
